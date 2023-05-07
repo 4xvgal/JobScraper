d69fc9dbbe9c451d5fdff66611051111894e1c34
HaeGwon <gwon99065@naver.com>
2023-05-07 16:40:34 +0900

    search

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
-#main.py
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
 
-print('hello world')
+browser = webdriver.Chrome()
 
-print('해권')
+browser.get('https://www.work.go.kr')
+browser.implicitly_wait(10)
+
+search = browser.find_element(By.ID, 'topQuery')
+search.click()
+
+search.send_keys('자바')
+search.send_keys(Keys.ENTER)
+
+wait = WebDriverWait(browser, 10)
+
+button = browser.find_element(By.CLASS_NAME, 'btn')
+button.click()
+
+#구인 정보
+items = browser.find_element(By.CLASS_NAME, 'top')
