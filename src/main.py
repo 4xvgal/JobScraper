d5e1b880cb5ead323cad0adac8ba8365d3af2d2e
HaeGwon <gwon99065@naver.com>
2023-05-07 16:50:59 +0900

    수정

--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.webdriver.support.ui import WebDriverWait
 
-options = webdriver.ChromeOptions()
-options.add_experimental_option("excludeSwitches", ["enable-logging"])
-browser = webdriver.Chrome(r'C:\program files\python310\chromedriver.exe')
+browser = webdriver.Chrome()
 
-browser.get('https://www.work.go.kr/seekWantedMain.do')
-browser.implicitly_wait(5)
+browser.get('https://www.work.go.kr')
+browser.implicitly_wait(10)
 
-first_sel = browser.find_element(By.ID ,'topQuery')
-first_sel.send_keys("벡엔드")
-first_sel.send_keys(Keys.ENTER)
-time.sleep(100)
-#third_sel = browser.find_element(By.CLASS_NAME ,'btn')
-#third_sel.click()
-second_sel = browser.find_element(By.CLASS_NAME,'line-dot')
-second_sel.click()
-print(second_sel.text)
+search = browser.find_element(By.ID, 'topQuery')
+search.click()
+
+search.send_keys('자바')
+search.send_keys(Keys.ENTER)
+
+wait = WebDriverWait(browser, 10)
+
+button = browser.find_element(By.CLASS_NAME, 'btn')
+button.click()
+
+#구인 정보
+items = browser.find_element(By.CLASS_NAME, 'top')
+
