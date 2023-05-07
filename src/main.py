c1cb7de6e1de6d279aacbf2173b4f7b9b92b8da7
hyenie1 <phc9811@naver.com>
2023-05-07 16:43:12 +0900

    d

--- src/main.py
+++ src/main.py
@@ -1,5 +1,21 @@
-#main.py
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 
-print('hello world')
+options = webdriver.ChromeOptions()
+options.add_experimental_option("excludeSwitches", ["enable-logging"])
+browser = webdriver.Chrome(r'C:\program files\python310\chromedriver.exe')
 
-print('해권')
+browser.get('https://www.work.go.kr/seekWantedMain.do')
+browser.implicitly_wait(5)
+
+first_sel = browser.find_element(By.ID ,'topQuery')
+first_sel.send_keys("벡엔드")
+first_sel.send_keys(Keys.ENTER)
+time.sleep(100)
+#third_sel = browser.find_element(By.CLASS_NAME ,'btn')
+#third_sel.click()
+second_sel = browser.find_element(By.CLASS_NAME,'line-dot')
+second_sel.click()
+print(second_sel.text)
