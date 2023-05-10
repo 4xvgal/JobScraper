6331c4b798e1ee58335613fdea7e7af7e769f805
4xvgal <4xvgal@protonmail.com>
2023-05-10 17:03:26 +0900

    added getPages function

--- src/test.py
+++ src/test.py
@@ -8,8 +8,7 @@ baseUrl = 'https://www.work.go.kr'  #워크넷의 url 저장
 browser = webdriver.Chrome()        #webdriver를 Chrome 으로 설정함
 
 browser.get(baseUrl)   #워크넷의 url를 get함수로 request 하여 browser에 저장한다.
-browser.implicitly_wait(10) #10초 기다리는 함수? - 주석처리필요 - 로딩완료시점까지만 wait 할수있는지 -jm
-
+browser.implicitly_wait(10) 
 search = browser.find_element(By.ID, 'topQuery')   #element 중 'topQuery'를 검색한다.
 search.click()                                      #검색한 element를 클릭한다
 
@@ -17,7 +16,7 @@ keyword = input("입력하세요")                           #검색할단어를
 search.send_keys(keyword)                             #keyword를 검색창에 전송한다
 search.send_keys(Keys.ENTER)                          #Enter 키를 전송한다
 
-wait = WebDriverWait(browser, 10)                       #10초간 기다린다. - 해당부분 로딩완료되는 시점까지만 wait 할수 있는지 궁금함 -jm
+wait = WebDriverWait(browser, 10)                       
 
 button = browser.find_element(By.XPATH, '//*[@id="contents"]/div[2]/div[1]/div[1]/div[3]/div[1]/div[2]/a')  
 button.click()
