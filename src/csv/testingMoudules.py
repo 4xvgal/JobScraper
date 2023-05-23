cf2e3b98ed416054997bcf2e3bbcc00897269654
4xvgal <4xvgal@protonmail.com>
2023-05-23 09:44:24 +0900

    최솟값 최대값 문제 해결

--- src/csv/testingMoudules.py
+++ src/csv/testingMoudules.py
@@ -7,20 +7,22 @@ import basicFunc as bf
 file_path = "src/csv/forTestFiles/data2.csv"
 export_path = "src/csv/forTestFiles/cleaned.csv"
 
+
 #월봉을 연봉으로 바꾸기
 with open(file_path, 'r',encoding='utf-8') as file:
     reader = csv.DictReader(file)
     cleaned_data = ce.changeMonthtoYear(reader)
-    bf.save_to_csv(cleaned_data,export_path,'utf-8',True)
+    bf.save_to_csv(cleaned_data,export_path,'utf-8',False)
 
-# # #이중데이터 (2000만 ~ 4000만) 데이터 분리 ()
-# with open(export_path,'r',encoding='utf-8') as file:
-#     reader = csv.DictReader(file)
-#     cleaned_data = ce.addMaxMinRow(reader)
-#     bf.save_to_csv(cleaned_data,export_path,'utf-8',True)
-
-# #근무지 데이터 간소화
+# #근무지 데이터 간소화 (연봉 이중데이터 전환보다 먼저 실행되어야함)
 with open(export_path,'r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     cleaned_data = ce.locationConv(reader)
+    bf.save_to_csv(cleaned_data,export_path,'utf-8',False)
+
+# #이중데이터 (2000만 ~ 4000만) 데이터 분리 ()
+with open(export_path,'r',encoding='utf-8') as file:
+    reader = csv.DictReader(file)
+    cleaned_data = ce.addMaxMinRow(reader)
     bf.save_to_csv(cleaned_data,export_path,'utf-8',True)
+
