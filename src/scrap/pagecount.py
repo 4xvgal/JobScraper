bfefaa557e7ae2624a30a6e671713c1af080d0a5
lee3604 <lee3604mine4@naver.com>
2023-05-24 11:14:03 +0900

    멀티스레드 함수

--- src/scrap/pagecount.py
+++ src/scrap/pagecount.py
@@ -1,5 +1,5 @@
  
-("test")
+
 def page_count(browser):
     total_items = browser.find_element(By.CSS_SELECTOR, 'span.font-size-14.font-cgray em.count').text
     total_items = int(total_items.replace(",",""))                       #1000 자리 넘어가면 문자열에 , 가 들어가면서 int 형 변환에러가 발생하므로 , 제거해 주는 코드 추가  -LGJ    23/05/17
