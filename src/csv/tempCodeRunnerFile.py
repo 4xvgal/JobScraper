1d5e44e496086805799bc06696b86ea15ba64419
4xvgal <4xvgal@protonmail.com>
2023-05-24 15:33:31 +0900

    csv 재가공 함수화 완료

--- src/csv/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-#근무지 데이터 간소화 (연봉 이중데이터 전환보다 먼저 실행되어야함)
-with open(export_path,'r', encoding='utf-8') as file:
-    reader = csv.DictReader(file)
-    cleaned_data = ce.locationConv(reader)
-    bf.save_to_csv(cleaned_data,export_path,'utf-8',False)
