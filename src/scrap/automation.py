58bb83ab96c9131abefeb06524fc8252818f1e07
lee3604 <lee3604mine4@naver.com>
2023-05-26 02:13:27 +0900

    거의 완성 80퍼

--- src/scrap/automation.py
+++ src/scrap/automation.py
@@ -1,67 +1,64 @@
 # 자동화 구현
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import certifi
-import os
+from selenium.webdriver.common.action_chains import ActionChains
 
-def auto_motive(url, word):
-   
 
-    def controller():
-        if url == "https://www.saramin.co.kr/":
-            return saramin()
-        else:
-            return worknet()
 
-    def saramin():
-        keyword = word  
 
-        browser = webdriver.Chrome()
-        browser.get(url)           
-        browser.implicitly_wait(30)                   
-        search = browser.find_element(By.ID, 'btn_search')  
-        search.click()
-        search = browser.find_element(By.CSS_SELECTOR, 'input.key')  
-        search.click()          
-        search.send_keys(keyword)                                                             
-        search.send_keys(Keys.ENTER)                      
-        wait = WebDriverWait(browser, 10)               
-        element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/ul[1]/li[2]/a'))) 
-        button = browser.find_element(By.XPATH, '//*[@id="content"]/ul[1]/li[2]/a')  
-        button.click()
+#사람인 웹브라우저를 실행하는 코드 
+def saramin(url, word):
 
-        variable = browser.current_url
+    keyword = word  
+    browser = webdriver.Chrome()                                  #webdriver를 Chrome 으로 설정함
+    browser.get(url)                                              #사람인의 url 저장
+    browser.implicitly_wait(10)                                   #최대 10초까지 기다리게 하는 함수 특정요소를 찾기 위해 10초 동안 대기. 시간 안에 찾아질시 즉시 중단. -HG
+    search = browser.find_element(By.ID, 'btn_search')            #검색 버튼을 찾아 상호작용. 사람인의 경우 검색 전 상호작용이 필요함
+    search.click()
+    search = browser.find_element(By.CSS_SELECTOR, 'input.key')   #상호 작용 후 검색창 클릭
+    search.click()                                                #검색한 element를 클릭한다
 
-        return browser, variable
+    search.send_keys(keyword)                                          #keyword를 검색창에 전송한다
+    search_button = browser.find_element(By.ID, 'btn_search_recruit')  #Enter키가 전송이 안되는 치명적 오류가 있어서 그냥 버튼누르는걸로 바꿨습니다..
+    actions = ActionChains(browser)
+    actions.move_to_element(search_button).click().perform()
+
+       
+    wait = WebDriverWait(browser, 10)                              #10초간 기다린다. - 해당부분 로딩완료되는 시점까지만 wait 할수 있는지 궁금함 -jm
+    element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="content"]/ul[1]/li[2]/a')))  
+    button = browser.find_element(By.XPATH, '//*[@id="content"]/ul[1]/li[2]/a')  
+    button.click()
+
+    variable = browser.current_url
+
+    return browser, variable
 
     
+#워크넷 웹브라우저를 실행하는 코드(이하 주석은 사람인과 동일)
+def worknet(url, word):                                            
+    keyword = word
+    browser = webdriver.Chrome()
+    browser.get(url)
+    browser.implicitly_wait(30)
+    search = browser.find_element(By.ID, 'topQuery')
+    search.click()
+    search.send_keys(keyword)
+    search.click()
+    search.send_keys(Keys.ENTER)
+    wait = WebDriverWait(browser, 10)
+    element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]/div[2]/div[1]/div[1]/div[3]/div[1]/div[2]/a')))
+    button = browser.find_element(By.XPATH, '//*[@id="contents"]/div[2]/div[1]/div[1]/div[3]/div[1]/div[2]/a')
+    button.click()
 
-    def worknet():
-        keyword = word
-        browser = webdriver.Chrome()
-        browser.get(url)
-        browser.implicitly_wait(30)
-        search = browser.find_element(By.ID, 'topQuery')
-        search.click()
-        search.send_keys(keyword)
-        search.click()
-        search.send_keys(Keys.ENTER)
-        wait = WebDriverWait(browser, 10)
-        element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="contents"]/div[2]/div[1]/div[1]/div[3]/div[1]/div[2]/a')))
-        button = browser.find_element(By.XPATH, '//*[@id="contents"]/div[2]/div[1]/div[1]/div[3]/div[1]/div[2]/a')
-        button.click()
+    variable = browser.current_url
 
-        variable = browser.current_url
+    print(variable)
 
-        print(variable)
+    return browser, variable
 
-        return browser, variable
-
-    return controller()
 
     
 
