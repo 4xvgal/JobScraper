2465e9e1e1c2bd02ee92d0e480f65a525466f4c5
lee3604 <lee3604mine4@naver.com>
2023-05-26 01:54:48 +0900

    70퍼 완성

--- src/scrap/crawling.py
+++ src/scrap/crawling.py
@@ -2,93 +2,104 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
+import os
 
-
-
-def get_worknet(page_index , route , veriable):
-
-    new_url = veriable.replace("pageIndex=1", f"pageIndex={page_index}")  # page_index 위치에 해당하는 페이지의 주소 new_url
-    response = requests.get(new_url)   # 각 페이지 마다 파싱(BeautifulSoup)
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    items = soup.select("li.cont-right")
+def get_info(page_index , url , route , veriable):
 
     
-    with open(route, 'a', encoding='CP949', newline='') as f:     #csv 파일생성
-        csvWriter = csv.writer(f)
 
-        for item in items:
-            try:
-                name = item.select_one('div.txt > span').get_text(strip=True)  # 딸려오는 개행문자 제거 - 에러수정   23/05/17
-            except AttributeError:
-                name = 'N/A'
+    def get_worknet():
 
-            try:
-                form = item.select_one('.cp-info > p > span:nth-child(1)').get_text(strip=True)
-            except AttributeError:
-                form = 'N/A'    
+        new_url = veriable.replace("pageIndex=1", f"pageIndex={page_index}")  # page_index 위치에 해당하는 페이지의 주소 new_url
+        response = requests.get(new_url)   # 각 페이지 마다 파싱(BeautifulSoup)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        items = soup.select("li.cont-right")
+        absolute_path = os.path.abspath(route[1])
 
-            try:
-               carrer = item.select_one('.cp-info > p > span:nth-child(2)').get_text(strip=True)
-               
-            except AttributeError:
-                carrer = 'N/A'
 
-            try:
-                salary = item.select_one('.cp-info > p > span:nth-child(4)').get_text(strip=True)
-               
-            except AttributeError:
-                salary = 'N/A'
+        with open(absolute_path, 'a', encoding='CP949', newline='') as f:     #csv 파일생성
+            csvWriter = csv.writer(f)
 
-            try:
-                locate = item.select_one('.cp-info > p:nth-child(2) > span:nth-child(1)').get_text(strip=True)
+            for item in items:
+                try:
+                    name = item.select_one('div.txt > span').get_text(strip=True)  # 딸려오는 개행문자 제거 - 에러수정   23/05/17
+                except AttributeError:
+                    name = 'N/A'
+
+                try:
+                    form = item.select_one('.cp-info > p > span:nth-child(1)').get_text(strip=True)
+                except AttributeError:
+                    form = 'N/A'    
+
+                try:
+                    carrer = item.select_one('.cp-info > p > span:nth-child(2)').get_text(strip=True)
                 
-            except AttributeError:
-                locate = 'N/A'
+                except AttributeError:
+                    carrer = 'N/A'
 
-            csvWriter.writerow([name, carrer, form, salary, locate])
-
-def get_saramin(page_index , route , veriable):
-
-    new_url = veriable.replace("recruitPage=1", f"recruitPage={page_index}")  # page_index 위치에 해당하는 페이지의 주소 new_url
-    response = requests.get(new_url)   # 각 페이지 마다 파싱(BeautifulSoup)
-    html = response.text
-    soup = BeautifulSoup(html, 'html.parser')
-    items = soup.select("div.content")
-
-    print(new_url)
-    
-
-    with open(route, 'a', encoding='UTF-8', newline='') as f:     #csv 파일생성
-        csvWriter = csv.writer(f)
-
-        for item in items:
-            try:
-                name = item.select_one('div.company-name a').get_text(strip=True)  # 딸려오는 개행문자 제거 - 에러수정   23/05/17
-            except AttributeError:
-                name = 'N/A'
-
-            try:
-                form = item.select_one('div.job_condition > span::nth-child(4)').get_text(strip=True)
-            except AttributeError:
-                form = 'N/A'    
-
-            try:
-                carrer = item.select_one('div.job_condition > span::nth-child(2)').get_text(strip=True)
-               
-            except AttributeError:
-                carrer = 'N/A'
-
-            try:
-                salary = item.select_one().get_text(strip=True)
-               
-            except AttributeError:
-                salary = 'N/A'
-
-            try:
-                locate = item.select_one().get_text(strip=True)
+                try:
+                    salary = item.select_one('.cp-info > p > span:nth-child(4)').get_text(strip=True)
                 
-            except AttributeError:
-                locate = 'N/A'
+                except AttributeError:
+                    salary = 'N/A'
 
-            csvWriter.writerow([name, carrer, form, salary, locate]) 
+                try:
+                    locate = item.select_one('.cp-info > p:nth-child(2) > span:nth-child(1)').get_text(strip=True)
+                    
+                except AttributeError:
+                    locate = 'N/A'
+
+                csvWriter.writerow([name, carrer, form, salary, locate])
+
+    def get_saramin():
+
+        new_url = veriable.replace("recruitPage=1", f"recruitPage={page_index}")  # page_index 위치에 해당하는 페이지의 주소 new_url
+        response = requests.get(new_url)   # 각 페이지 마다 파싱(BeautifulSoup)
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        items = soup.select("div.content")
+        absolute_path = os.path.abspath(route[0])
+
+        print(route[0])
+        
+
+        with open(absolute_path, 'a', encoding='UTF-8', newline='') as f:     #csv 파일생성
+            csvWriter = csv.writer(f)
+
+            for item in items:
+                try:
+                    name = item.select_one('div.company-name a').get_text(strip=True)  # 딸려오는 개행문자 제거 - 에러수정   23/05/17
+                except AttributeError:
+                    name = 'N/A'
+
+                try:
+                    form = item.select_one('div.job_condition > span::nth-child(4)').get_text(strip=True)
+                except AttributeError:
+                    form = 'N/A'    
+
+                try:
+                    carrer = item.select_one('div.job_condition > span::nth-child(2)').get_text(strip=True)
+                
+                except AttributeError:
+                    carrer = 'N/A'
+
+                try:
+                    salary = item.select_one().get_text(strip=True)
+                
+                except AttributeError:
+                    salary = 'N/A'
+
+                try:
+                    locate = item.select_one().get_text(strip=True)
+                    
+                except AttributeError:
+                    locate = 'N/A'
+
+                csvWriter.writerow([name, carrer, form, salary, locate]) 
+
+
+    if(url == 'https://www.saramin.co.kr/'):
+        get_saramin()
+    else:
+        get_worknet()
