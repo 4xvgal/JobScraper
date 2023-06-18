3a15783647f8b387e093a29744ce747df0675066
4xvgal <4xvgal@protonmail.com>
2023-06-18 22:06:15 +0900

    [UPDATE] 작성중인 함수 추가

--- src/gui/gui/mainwindow.py
+++ src/gui/gui/mainwindow.py
@@ -1,13 +1,37 @@
 # This Python file uses the following encoding: utf-8
 import sys
-
+import csv
+import pandas as pd
 from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtCore import Qt, QAbstractTableModel
 
 # Important:
 # You need to run the following command to generate the ui_form.py file
 #     pyside6-uic form.ui -o ui_form.py, or
 #     pyside2-uic form.ui -o ui_form.py
 from ui_form import Ui_MainWindow
+filePath = "src/csv/forTestFiles/cleaned.csv"
+
+#CSV 데이터 저장형식 클래스
+class CSVTableModel(QAbstractTableModel):
+    def __init__(self, data):
+        super().__init__()
+        self.data = data
+
+    def rowCount(self, parent):
+        return len(self.data)
+
+    def columnCount(self, parent):
+        if self.data is not None:
+            return len(self.data.columns)
+        return 0
+
+    def data(self, index, role):
+        if role == Qt.ItemDataRole.DisplayRole:
+            row = index.row()
+            col = index.column()
+            return str(self.data.iloc[row, col])
+        return None
 #주 윈도우 클래스
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -16,14 +40,20 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
 
         #검색버튼을 눌렀을때
-        self.ui.search_button.clicked.connect(self.testclick)
+        self.ui.search_button.clicked.connect(self.initSearch)
     #검색 버튼 클릭되었을때
-    def testclick(self):
-        print("hello")
+    def initSearch(self):
+        global filePath
+        # Read CSV file and retrieve the data
+        data = []
+        with open(filePath, 'r',encoding='UTF-8') as file:
+            csv_reader = csv.reader(file)
+            for row in csv_reader:
+                data.append(row)
 
-def loadingCSV(file):
-    print("hello")
-
+        # Create a model and set it to the table view
+        model = CSVTableModel(data)
+        self.ui.ShowingCSV.setModel(model)
 #실행부분
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- src/gui/gui/ui_form.py
+++ src/gui/gui/ui_form.py
@@ -18,7 +18,7 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
 from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QHeaderView,
     QLabel, QMainWindow, QPushButton, QSizePolicy,
     QStatusBar, QTabWidget, QTableView, QTextEdit,
-    QWidget)
+    QVBoxLayout, QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -27,12 +27,6 @@ class Ui_MainWindow(object):
         MainWindow.resize(800, 600)
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
-        self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(10, 10, 141, 16))
-        font = QFont()
-        font.setFamilies([u"Nanum Gothic"])
-        self.label.setFont(font)
         self.tabWidget = QTabWidget(self.centralwidget)
         self.tabWidget.setObjectName(u"tabWidget")
         self.tabWidget.setGeometry(QRect(20, 80, 731, 441))
@@ -45,9 +39,28 @@ class Ui_MainWindow(object):
         self.Statics = QWidget()
         self.Statics.setObjectName(u"Statics")
         self.tabWidget.addTab(self.Statics, "")
-        self.frame = QFrame(self.centralwidget)
+        self.verticalLayoutWidget = QWidget(self.centralwidget)
+        self.verticalLayoutWidget.setObjectName(u"verticalLayoutWidget")
+        self.verticalLayoutWidget.setGeometry(QRect(0, 0, 160, 31))
+        self.verticalLayout = QVBoxLayout(self.verticalLayoutWidget)
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
+        self.label = QLabel(self.verticalLayoutWidget)
+        self.label.setObjectName(u"label")
+        font = QFont()
+        font.setFamilies([u"Nanum Gothic"])
+        self.label.setFont(font)
+
+        self.verticalLayout.addWidget(self.label)
+
+        self.verticalLayoutWidget_2 = QWidget(self.centralwidget)
+        self.verticalLayoutWidget_2.setObjectName(u"verticalLayoutWidget_2")
+        self.verticalLayoutWidget_2.setGeometry(QRect(160, 0, 261, 80))
+        self.verticalLayout_2 = QVBoxLayout(self.verticalLayoutWidget_2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.frame = QFrame(self.verticalLayoutWidget_2)
         self.frame.setObjectName(u"frame")
-        self.frame.setGeometry(QRect(150, 0, 271, 80))
         self.frame.setFrameShape(QFrame.StyledPanel)
         self.frame.setFrameShadow(QFrame.Raised)
         self.search_button = QPushButton(self.frame)
@@ -76,14 +89,25 @@ class Ui_MainWindow(object):
         self.label_3.setObjectName(u"label_3")
         self.label_3.setGeometry(QRect(0, 50, 91, 16))
         self.label_3.setFont(font)
-        self.frame_2 = QFrame(self.centralwidget)
+
+        self.verticalLayout_2.addWidget(self.frame)
+
+        self.verticalLayoutWidget_3 = QWidget(self.centralwidget)
+        self.verticalLayoutWidget_3.setObjectName(u"verticalLayoutWidget_3")
+        self.verticalLayoutWidget_3.setGeometry(QRect(430, 0, 281, 80))
+        self.verticalLayout_3 = QVBoxLayout(self.verticalLayoutWidget_3)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.verticalLayout_3.setContentsMargins(0, 0, 0, 0)
+        self.frame_2 = QFrame(self.verticalLayoutWidget_3)
         self.frame_2.setObjectName(u"frame_2")
-        self.frame_2.setGeometry(QRect(430, 0, 271, 80))
         self.frame_2.setFrameShape(QFrame.StyledPanel)
         self.frame_2.setFrameShadow(QFrame.Raised)
         self.pushButton = QPushButton(self.frame_2)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(10, 0, 100, 32))
+        self.pushButton.setGeometry(QRect(0, 0, 100, 32))
+
+        self.verticalLayout_3.addWidget(self.frame_2)
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -99,9 +123,9 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"JobScraperGUI", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"\uad6c\uc778\uad6c\uc9c1 \uc815\ubcf4 \ud1b5\ud569 \uac80\uc0c9", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.ListViewer), QCoreApplication.translate("MainWindow", u"ListViewer", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.Statics), QCoreApplication.translate("MainWindow", u"\ud1b5\uacc4\uce58\ubcf4\uae30", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\uad6c\uc778\uad6c\uc9c1 \uc815\ubcf4 \ud1b5\ud569 \uac80\uc0c9", None))
         self.search_button.setText(QCoreApplication.translate("MainWindow", u"\uac80\uc0c9", None))
         self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"8 (\uae30\ubcf8\uac12)", None))
         self.comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"1", None))
