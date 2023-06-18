1fe81e769ff3ce9cc4ee2e2396b192e985bd47b2
4xvgal <4xvgal@protonmail.com>
2023-06-18 15:36:41 +0900

    [UPDATE] MINOR FIX

--- src/gui/gui/ui_form.py
+++ src/gui/gui/ui_form.py
@@ -11,13 +11,15 @@
 from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
     QMetaObject, QObject, QPoint, QRect,
     QSize, QTime, QUrl, Qt)
-from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
-    QFont, QFontDatabase, QGradient, QIcon,
-    QImage, QKeySequence, QLinearGradient, QPainter,
-    QPalette, QPixmap, QRadialGradient, QTransform)
-from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenuBar,
-    QPushButton, QSizePolicy, QStatusBar, QTabWidget,
-    QTextEdit, QWidget)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QComboBox, QFrame, QLabel,
+    QMainWindow, QMenu, QMenuBar, QPushButton,
+    QSizePolicy, QStatusBar, QTabWidget, QTextEdit,
+    QWidget)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -28,44 +30,74 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName(u"centralwidget")
         self.label = QLabel(self.centralwidget)
         self.label.setObjectName(u"label")
-        self.label.setGeometry(QRect(30, 10, 141, 16))
+        self.label.setGeometry(QRect(10, 10, 141, 16))
         font = QFont()
         font.setFamilies([u"Nanum Gothic"])
         self.label.setFont(font)
         self.tabWidget = QTabWidget(self.centralwidget)
         self.tabWidget.setObjectName(u"tabWidget")
-        self.tabWidget.setGeometry(QRect(30, 50, 731, 381))
+        self.tabWidget.setGeometry(QRect(20, 80, 731, 441))
         self.ListViewer = QWidget()
         self.ListViewer.setObjectName(u"ListViewer")
         self.tabWidget.addTab(self.ListViewer, "")
         self.Statics = QWidget()
         self.Statics.setObjectName(u"Statics")
         self.tabWidget.addTab(self.Statics, "")
-        self.textEdit = QTextEdit(self.centralwidget)
-        self.textEdit.setObjectName(u"textEdit")
-        self.textEdit.setGeometry(QRect(260, 10, 104, 21))
-        self.label_2 = QLabel(self.centralwidget)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(180, 10, 71, 16))
-        self.label_2.setFont(font)
-        self.search_button = QPushButton(self.centralwidget)
+        self.frame = QFrame(self.centralwidget)
+        self.frame.setObjectName(u"frame")
+        self.frame.setGeometry(QRect(150, 0, 271, 80))
+        self.frame.setFrameShape(QFrame.StyledPanel)
+        self.frame.setFrameShadow(QFrame.Raised)
+        self.search_button = QPushButton(self.frame)
         self.search_button.setObjectName(u"search_button")
-        self.search_button.setGeometry(QRect(380, 10, 51, 32))
-        self.pushButton = QPushButton(self.centralwidget)
+        self.search_button.setGeometry(QRect(190, 1, 61, 41))
+        self.comboBox = QComboBox(self.frame)
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.addItem("")
+        self.comboBox.setObjectName(u"comboBox")
+        self.comboBox.setGeometry(QRect(100, 40, 111, 31))
+        self.textEdit = QTextEdit(self.frame)
+        self.textEdit.setObjectName(u"textEdit")
+        self.textEdit.setGeometry(QRect(80, 10, 104, 21))
+        self.label_2 = QLabel(self.frame)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 10, 71, 16))
+        self.label_2.setFont(font)
+        self.label_3 = QLabel(self.frame)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(0, 50, 91, 16))
+        self.label_3.setFont(font)
+        self.frame_2 = QFrame(self.centralwidget)
+        self.frame_2.setObjectName(u"frame_2")
+        self.frame_2.setGeometry(QRect(430, 0, 271, 80))
+        self.frame_2.setFrameShape(QFrame.StyledPanel)
+        self.frame_2.setFrameShadow(QFrame.Raised)
+        self.pushButton = QPushButton(self.frame_2)
         self.pushButton.setObjectName(u"pushButton")
-        self.pushButton.setGeometry(QRect(480, 10, 100, 32))
+        self.pushButton.setGeometry(QRect(10, 0, 100, 32))
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(MainWindow)
         self.menubar.setObjectName(u"menubar")
         self.menubar.setGeometry(QRect(0, 0, 800, 24))
+        self.menu = QMenu(self.menubar)
+        self.menu.setObjectName(u"menu")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
         MainWindow.setStatusBar(self.statusbar)
 
+        self.menubar.addAction(self.menu.menuAction())
+
         self.retranslateUi(MainWindow)
 
-        self.tabWidget.setCurrentIndex(1)
+        self.tabWidget.setCurrentIndex(0)
 
 
         QMetaObject.connectSlotsByName(MainWindow)
@@ -76,8 +108,20 @@ class Ui_MainWindow(object):
         self.label.setText(QCoreApplication.translate("MainWindow", u"\uad6c\uc778\uad6c\uc9c1 \uc815\ubcf4 \ud1b5\ud569 \uac80\uc0c9", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.ListViewer), QCoreApplication.translate("MainWindow", u"ListViewer", None))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.Statics), QCoreApplication.translate("MainWindow", u"\ud1b5\uacc4\uce58\ubcf4\uae30", None))
-        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\uac80\uc0c9\uc5b4 \uc785\ub825", None))
         self.search_button.setText(QCoreApplication.translate("MainWindow", u"\uac80\uc0c9", None))
-        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.comboBox.setItemText(0, QCoreApplication.translate("MainWindow", u"8 (\uae30\ubcf8\uac12)", None))
+        self.comboBox.setItemText(1, QCoreApplication.translate("MainWindow", u"1", None))
+        self.comboBox.setItemText(2, QCoreApplication.translate("MainWindow", u"2", None))
+        self.comboBox.setItemText(3, QCoreApplication.translate("MainWindow", u"3", None))
+        self.comboBox.setItemText(4, QCoreApplication.translate("MainWindow", u"4", None))
+        self.comboBox.setItemText(5, QCoreApplication.translate("MainWindow", u"5", None))
+        self.comboBox.setItemText(6, QCoreApplication.translate("MainWindow", u"6", None))
+        self.comboBox.setItemText(7, QCoreApplication.translate("MainWindow", u"7", None))
+        self.comboBox.setItemText(8, QCoreApplication.translate("MainWindow", u"New Item", None))
+
+        self.label_2.setText(QCoreApplication.translate("MainWindow", u"\uac80\uc0c9\uc5b4 \uc785\ub825", None))
+        self.label_3.setText(QCoreApplication.translate("MainWindow", u"\uba40\ud2f0\ud504\ub85c\uc138\uc11c \uc218", None))
+        self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\uac80\uc0c9\uacb0\uacfc \ucd08\uae30\ud654", None))
+        self.menu.setTitle(QCoreApplication.translate("MainWindow", u"\uba54\ub274", None))
     # retranslateUi
 
--- src/scrap/main.py
+++ src/scrap/main.py
@@ -5,18 +5,18 @@ from startfunction import start
 import time
 import multiprocessing
 
-if __name__ == '__main__':
-    
-    #start 에 전달 할 요소들
-    start_time = time.time()
-    url = ["https://www.saramin.co.kr/" , "https://www.work.go.kr"]
-    route = ["C:\CSV\saramin_data.csv" , "C:\CSV\worknet_data.csv"]
-    keyword = input("입력하세요 : ")
 
-    pool = multiprocessing.Pool(processes=8)                            #16개 프로세스를 호출하였습니다.. 추가 할 수 있는데 컴퓨터 성능에 따라 달라집니다
-    pool.starmap(start, [(address, route, keyword) for address in url])  #주소 , 경로 , 키워드를 준다.
-    pool.close()                                                         # 테스트에 사용한 키워드는 '백엔드' 89개 회사 정보를 크롤링합니다. 기존 코드 30초에서 20초로 10 초 단축시켰습니다.
+def run_crawling(keyword, processCount, SavingDir): #검색어, 멀티프로세서 수, 저장경로
+    start_time = time.time()
+    url = ["https://www.saramin.co.kr/", "https://www.work.go.kr"]
+    route = ["C:\CSV\saramin_data.csv", "C:\CSV\worknet_data.csv"]
+
+    pool = multiprocessing.Pool(processCount=8)
+    pool.starmap(start, [(address, route, keyword) for address in url])
+    pool.close()
     pool.join()
 
-    print("--- %s seconds ---" % (time.time() - start_time)) # 속도 측정용 코드입니다 ..   
-    
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+if __name__ == '__main__':
+    run_crawling()
