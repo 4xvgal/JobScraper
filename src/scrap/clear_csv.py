6b22900a9df3d511860ab20978f9e00dfea27ab8
4xvgal <4xvgal@protonmail.com>
2023-06-21 17:21:38 +0900

    [BUGFIX] clear_csv 에서 공백화 기능 삭제

--- src/scrap/clear_csv.py
+++ src/scrap/clear_csv.py
@@ -10,27 +10,27 @@ def Initialization(route, final_route, merged):
     saramin_final= final_route[0]
     worknet_final= final_route[1]
 
-    with open(saramin, 'w') as file:
-        pass
+    # with open(saramin, 'w') as file:
+    #     pass
 
     os.remove(saramin)
 
-    with open(worknet, 'w') as file:
-        pass
+    # with open(worknet, 'w') as file:
+    #     pass
 
     os.remove(worknet)
 
-    with open(saramin_final, 'w') as file:
-        pass
+    # with open(saramin_final, 'w') as file:
+    #     pass
 
     os.remove(saramin_final)
 
-    with open(worknet_final, 'w') as file:
-        pass
+    # with open(worknet_final, 'w') as file:
+    #     pass
 
     os.remove(worknet_final)
 
-    with open(merged, 'w') as file:
-        pass
+    # with open(merged, 'w') as file:
+    #     pass
 
     os.remove(merged)
