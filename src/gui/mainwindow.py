78de57ea93d04ae5bb155313c765a2d5490826b5
HaeGwon <gwon99065@naver.com>
2023-06-21 16:25:41 +0900

    init파일 제거

--- src/gui/mainwindow.py
+++ src/gui/mainwindow.py
@@ -5,7 +5,7 @@ from multiprocessing import Process
 from PySide6.QtWidgets import QApplication, QMainWindow
 from PySide6.QtCore import Qt, QAbstractTableModel
 from PySide6.QtCore import QTimer
-
+from 
 
 
 #다른 코드들 import
